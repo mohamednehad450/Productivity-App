fd2b5e8b11660dccb43f8dbb8bba80898f009673
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-13 00:03:02 +0300

    feat: Remove auth

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -4,18 +4,6 @@ import HabitTracker from "./HabitTracker";
 import Settings from "./Settings";
 import TodoList from "./TodoList";
 import Graphs from "./Graphs";
-import SignIn from "./SignIn";
-import Register from "./Register";
 import { useQuery } from "./hooks";
 
-export {
-  routes,
-  Pomodoro,
-  TodoList,
-  Graphs,
-  HabitTracker,
-  Settings,
-  SignIn,
-  Register,
-  useQuery,
-};
+export { routes, Pomodoro, TodoList, Graphs, HabitTracker, Settings, useQuery };
